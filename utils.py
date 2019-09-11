f19c119a1ceba70ec5704e54ffd7c748fe8d1d84
ktcranme <31867995+ktcranme@users.noreply.github.com>
2019-09-11 10:58:32 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -15,7 +15,8 @@ def getListOfImageFilenames(filePath):
 #returns 3D matrix
 #Dimensions: (x), (y), (tuple of RGB)
 def getRGBImage(imageName):
-	image = cv2.imread(imageName)
+	image = cv2.imread(imageName, cv2.IMREAD_COLOR)
+	image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 	return image
 
 #returns 3D matrix
@@ -29,7 +30,7 @@ def getYUVImage(imageName):
 	return image
 
 def getGreyScaleImage(imageName):
-	image = cv2.imread(imageName, cv2.IMREAD_GRAYSCALE)
+	image = cv2.imread(imageName, 0)
 	return image
 
 #gets the feature vector for HOG
@@ -38,6 +39,14 @@ def getHOGFD(image):
 	#plt.imshow(hog_image, cmap=plt.cm.gray) 
 	#plt.show()
 	return fd
+
+#returns a flattened feature vector of size K x 128 where
+#K is the number of keypoints found on the image
+def getSIFTFD(image, sift):
+		kp, des = sift.detectAndCompute(image,None)
+		#print(des.shape)
+		return des.flatten()
+
 
 def downScaleImage(image):
 	return block_reduce(image, block_size=(10, 10), func=np.mean)
@@ -55,6 +64,28 @@ def compareTwoHOG(fd1, fd2):
 	l = np.sum(l)
 	l = np.sqrt(l)
 	return l
+
+#matchCountWeight is for how much to weigh the amount
+#of matches, 1 to only care about number of matches, 0 to only care about match
+#distance average (not implemented yet)
+#threshold is ...
+def compareTwoSIFT(des1, des2, threshold=0.8, matchCountWeight=0.5):
+	# BFMatcher with default params
+	bf = cv2.BFMatcher()
+	matches = bf.knnMatch(des1,des2, k=2)
+
+	# Apply ratio test
+	good = []
+	for m,n in matches:
+		if m.distance < 0.8*n.distance:
+			good.append([m.distance])
+
+	good = np.array(good)
+	distance = np.sqrt(np.square(good).sum()) #square them to punish extreme differences
+	distanceAvg = (distance / len(good))
+
+
+	return distanceAvg
 
 #returns an array containing the names for n most similar
 #images contained in data
@@ -85,6 +116,35 @@ def findNClosestHOG(fd, data, n):
 	for i in ind:
 		result.append(data[i][0])
 		print(summed[i])
+
+	return result
+
+
+def findNClosestSIFT(fd, data, fileNames, n):
+	npData = np.array(data)
+
+	distances = []
+	for fd2 in data:
+		distances.append(compareTwoSIFT(fd, fd2))
+
+	distances = np.array(distances)
+	print("looking for", n, "closest images...");
+
+	#sorts the array, takes a bit to do
+	ind = np.argpartition(distances, n+1)[:n+1]
+	ind = ind[np.argsort(distances[ind])]
+
+	#if chosen image is in data still
+	#prune it from list here
+	if(distances[ind[0]] == 0):
+		ind = ind[1:]
+	else:
+		ind = ind[:-1]
+
+	#get list of image names instead of indices
+	result = []
+	for i in ind:
+		result.append(fileNames[i])
 
 	return result
 
