0365a25a021ec6377b8cee5d9b0af18a3bc4d79d
ktcranme <31867995+ktcranme@users.noreply.github.com>
2019-09-11 10:59:36 -0700

    Update proj1.py

--- proj1.py
+++ proj1.py
@@ -1,22 +1,29 @@
 import utils
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
 
 
 filePath = '../Images/Hands/'
 files = utils.getListOfImageFilenames(filePath)
-outfile = '../CSV/HOGLarge.csv'
+outfile = '../CSV/SIFTLarge.csv'
 
+"""
+plt.imshow(utils.downScaleImage(image))
+plt.show()
+"""
 
+# Initiate SIFT detector
+sift = cv2.xfeatures2d.SIFT_create()
 data = []
 for file in files:
 	if(file == '.DS_Store'):
 		continue
 	image = utils.getGreyScaleImage(filePath + file)
-	downscaledImage = utils.downScaleImage(image)
+	#downscaledImage = utils.downScaleImage(image)
 	entry = []
 	entry.append(file)
-	fd = utils.getHOGFD(downscaledImage)
+	fd = utils.getSIFTFD(image, sift)
 	entry.extend(fd)
 	data.append(entry)
 
